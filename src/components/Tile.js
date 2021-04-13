3fa522b1b6962152733b36aeda15bcc97cb9a3c5
Abhi Motgi <apmotgi16@gmail.com>
2021-04-13 00:41:25 -0400

    fixed row height

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -2,6 +2,10 @@ import classnames from 'classnames';
 
 
 export default function Tile(props) {
+    // return (
+    //     <button className={classnames(props.className, props.active, props.value)} onClick={props.onClick}>
+    //     </button>
+    // );
     return (
         <button className={classnames(props.className, props.active, props.value)} onClick={props.onClick}>
         </button>
